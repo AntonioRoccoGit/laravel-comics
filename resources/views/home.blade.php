d6b3c7e3fe773182edfe5f9ccc7f634c43d370f0
AntonioRoccoGit <antonio.rocco97@gmail.com>
2023-05-23 13:33:58 +0200

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app-layout')
+
+@section('content')
+    {{-- @foreach ($comics_array as $comic)
+        <p>
+            {{ $comic['title'] }}
+        </p>
+    @endforeach --}}
+@endsection
